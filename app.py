734a175820e8264be57c95271471799e211e5e5a
Andrews3002 <alexangeloandrews@gmail.com>
2024-11-14 12:14:06 -0400

    port update

--- app.py
+++ app.py
@@ -346,7 +346,8 @@ def favorites_page():
   return render_template('Favorites.html', Companies=Companies, q=q, page=page)
   
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',  port=int(os.environ.get("PORT", 5000)))
+  port = int(os.environ.get("PORT", 5000))
+  app.run(host="0.0.0.0", port=port)
 
 
 
